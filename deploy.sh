27e8549f34fafa70beb05b807b8c59231958f84a
madhum-15 <80889919+madhum-15@users.noreply.github.com>
2021-03-22 00:17:43 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,7 @@
 
 netstat -plant | grep 8001 | grep Listen
 if [ $? != 0 ]; then
-python3.7 manage.py runserver 0.0.0.0:8001 &
-exit 0
+python3.7 manage.py runserver 0.0.0.0:8001
 else
 echo "Application already running"
 fi
