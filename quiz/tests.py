789e23a3a3de9b9a3043baab8c4da77aad3418d9
madhum-15 <80889919+madhum-15@users.noreply.github.com>
2021-03-24 21:05:39 +0530

    Update tests.py

--- quiz/tests.py
+++ quiz/tests.py
@@ -25,3 +25,19 @@ class MyTests(TestCase):
     def test_transaction_behavior(self):
         # ... conditional test code
         pass
+import random
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+class SimpleTest(unittest.TestCase):
+    @unittest.skip("demonstrating skipping")
+    def test_skipped(self):
+        self.fail("shouldn't happen")
+
+    def test_pass(self):
+        self.assertEqual(10, 7 + 3)
+
+    def test_fail(self):
+        self.assertEqual(11, 7 + 3)
