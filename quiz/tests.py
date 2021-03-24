5ebcb2916870c4761942bf3a0ca8dbb8b617d4d7
madhum-15 <80889919+madhum-15@users.noreply.github.com>
2021-03-24 12:45:42 +0530

    Update tests.py

--- quiz/tests.py
+++ quiz/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.test import tag
+import unittest
+class SampleTest(unittest.TestCase):
+   # return True or False
+   def test(self):
+      self.assertTrue(True)
+# running the test
+unittest.main()
+class SampleTestCase(TestCase):
 
-## Create your tests here.
+    @tag('fast')
+    def test_fast(self):
+        ...
+
+    @tag('slow')
+    def test_slow(self):
+        ...
+
+    @tag('slow', 'core')
+    def test_slow_but_core(self):
+        ...
+class MyTests(TestCase):
+    @skipIfDBFeature('supports_transactions')
+    def test_transaction_behavior(self):
+        # ... conditional test code
+        pass
