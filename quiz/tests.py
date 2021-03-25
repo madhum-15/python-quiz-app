c78c33dbb9ca44c34e46181e2b6b9cd069cff3d0
madhum-15 <80889919+madhum-15@users.noreply.github.com>
2021-03-25 14:57:24 +0530

    Update tests.py

--- quiz/tests.py
+++ quiz/tests.py
@@ -6,38 +6,5 @@ class SampleTest(unittest.TestCase):
    def test(self):
       self.assertTrue(True)
 # running the test
-unittest.main()
-class SampleTestCase(TestCase):
-
-    @tag('fast')
-    def test_fast(self):
-        ...
-
-    @tag('slow')
-    def test_slow(self):
-        ...
-
-    @tag('slow', 'core')
-    def test_slow_but_core(self):
-        ...
-class MyTests(TestCase):
-    @skipIfDBFeature('supports_transactions')
-    def test_transaction_behavior(self):
-        # ... conditional test code
-        pass
-import random
-try:
-    import unittest2 as unittest
-except ImportError:
-    import unittest
-
-class SimpleTest(unittest.TestCase):
-    @unittest.skip("demonstrating skipping")
-    def test_skipped(self):
-        self.fail("shouldn't happen")
-
-    def test_pass(self):
-        self.assertEqual(10, 7 + 3)
-
-    def test_fail(self):
-        self.assertEqual(11, 7 + 3)
+if __name__ == '__main__':
+    unittest.main()
